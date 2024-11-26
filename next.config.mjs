dcf7ed22478fdb541f46fe8e0b8afd3187704f57
onodanaoto <onoda@rakudasystem.co.jp>
2024-11-27 08:49:16 +0900

    Build successful, ready for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'standalone',
-  experimental: {
-    outputStandalone: true
-  }
+  output: 'standalone'
 };
 
 export default nextConfig;
